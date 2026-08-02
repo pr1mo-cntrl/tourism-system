7b5c0781d4169c1ba54b1b017e1cad15b50c907e
Jan Kaisser F. Dulawan <jfd5349@students.uc-bcf.edu.ph>
2026-08-02 18:23:41 +0800

    Force HTTPS for Render deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
     }
 
     /**
