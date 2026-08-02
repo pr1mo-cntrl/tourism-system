513a0c7b97ef82f08648859a53a7da4cfc907537
Jan Kaisser F. Dulawan <jfd5349@students.uc-bcf.edu.ph>
2026-08-02 18:27:57 +0800

    Fix bracket syntax

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,21 +7,15 @@ use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
