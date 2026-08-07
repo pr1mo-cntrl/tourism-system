6407176e4724903a417110ff809e937b0dcbf670
Jan Kaisser F. Dulawan <jfd5349@students.uc-bcf.edu.ph>
2026-08-07 15:17:38 +0800

    Use native scrollable form with placeholder headings

--- app/Filament/Resources/Accommodations/AccommodationResource.php
+++ app/Filament/Resources/Accommodations/AccommodationResource.php
@@ -10,6 +10,7 @@ use Filament\Schemas\Schema;
 use Filament\Tables\Table;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Placeholder;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 
@@ -25,140 +26,137 @@ class AccommodationResource extends Resource
     {
         return $schema
             ->components([
-                \Filament\Forms\Components\Tabs::make('Accommodation Details')
-                    ->tabs([
-                        
-                        // TAB 1: ESTABLISHMENT INFO
-                        \Filament\Forms\Components\Tabs\Tab::make('Establishment Info')
-                            ->schema([
-                                Select::make('name') 
-                                    ->label('Name of Establishment')
-                                    ->searchable()
-                                    ->live()
-                                    ->afterStateUpdated(function ($state, callable $set) {
-                                        $mapping = [
-                                            'SUNRISE CABIN TRAVELLERS INN' => 'Atok',
-                                            'ISAAC JORDAN TRANSIENT' => 'Atok',
-                                        ];
-                                        if (isset($mapping[$state])) {
-                                            $set('municipality', $mapping[$state]);
-                                        }
-                                    })
-                                    ->options([
-                                        'SUNRISE CABIN TRAVELLERS INN' => 'SUNRISE CABIN TRAVELLERS INN',
-                                        'ISAAC JORDAN TRANSIENT' => 'ISAAC JORDAN TRANSIENT',
-                                    ])
-                                    ->required(),    
+                // PART 1: ESTABLISHMENT INFO
+                Placeholder::make('heading_info')
+                    ->label('ESTABLISHMENT INFORMATION')
+                    ->content('Enter the basic details of the establishment, rooms, and employees.'),
 
-                                TextInput::make('year')
-                                    ->label('Year')
-                                    ->numeric()
-                                    ->default(2026)
-                                    ->minValue(1900)
-                                    ->maxValue(2100),
+                Select::make('name') 
+                    ->label('Name of Establishment')
+                    ->searchable()
+                    ->live()
+                    ->afterStateUpdated(function ($state, callable $set) {
+                        $mapping = [
+                            'SUNRISE CABIN TRAVELLERS INN' => 'Atok',
+                            'ISAAC JORDAN TRANSIENT' => 'Atok',
+                        ];
+                        if (isset($mapping[$state])) {
+                            $set('municipality', $mapping[$state]);
+                        }
+                    })
+                    ->options([
+                        'SUNRISE CABIN TRAVELLERS INN' => 'SUNRISE CABIN TRAVELLERS INN',
+                        'ISAAC JORDAN TRANSIENT' => 'ISAAC JORDAN TRANSIENT',
+                    ])
+                    ->required(),    
 
-                                Select::make('month')
-                                    ->label('Month')
-                                    ->options([
-                                        'JANUARY' => 'January', 'FEBRUARY' => 'February', 'MARCH' => 'March', 
-                                        'APRIL' => 'April', 'MAY' => 'May', 'JUNE' => 'June', 
-                                        'JULY' => 'July', 'AUGUST' => 'August', 'SEPTEMBER' => 'September', 
-                                        'OCTOBER' => 'October', 'NOVEMBER' => 'November', 'DECEMBER' => 'December'
-                                    ])
-                                    ->searchable()
-                                    ->required(),
+                TextInput::make('year')
+                    ->label('Year')
+                    ->numeric()
+                    ->default(2026)
+                    ->minValue(1900)
+                    ->maxValue(2100),
 
-                                TextInput::make('province')
-                                    ->label('Province/HUC/ICC (*Region)')
-                                    ->default('Benguet')
-                                    ->required(),
+                Select::make('month')
+                    ->label('Month')
+                    ->options([
+                        'JANUARY' => 'January', 'FEBRUARY' => 'February', 'MARCH' => 'March', 
+                        'APRIL' => 'April', 'MAY' => 'May', 'JUNE' => 'June', 
+                        'JULY' => 'July', 'AUGUST' => 'August', 'SEPTEMBER' => 'September', 
+                        'OCTOBER' => 'October', 'NOVEMBER' => 'November', 'DECEMBER' => 'December'
+                    ])
+                    ->searchable()
+                    ->required(),
 
-                                Select::make('municipality')
-                                    ->label('Municipality')
-                                    ->options([
-                                        'Atok' => 'Atok', 'Bakun' => 'Bakun', 'Bokod' => 'Bokod', 
-                                        'Buguias' => 'Buguias', 'Itogon' => 'Itogon', 'Kabayan' => 'Kabayan', 
-                                        'Kapangan' => 'Kapangan', 'Kibungan' => 'Kibungan', 'La Trinidad' => 'La Trinidad', 
-                                        'Mankayan' => 'Mankayan', 'Sablan' => 'Sablan', 'Tuba' => 'Tuba', 'Tublay' => 'Tublay',
-                                    ])
-                                    ->searchable()
-                                    ->required(),
+                TextInput::make('province')
+                    ->label('Province/HUC/ICC (*Region)')
+                    ->default('Benguet')
+                    ->required(),
 
-                                Select::make('type')
-                                    ->label('Accommodation Type')
-                                    ->options([
-                                        'HTL' => 'HTL - Hotel', 'RES' => 'RES - Resort', 'TIN' => 'TIN - Tourist Inn', 
-                                        'APA' => 'APA - Apartel', 'PEN' => 'PEN - Pension House', 'HSS' => 'HSS - Homestay', 'OTH' => 'OTH - Others',
-                                    ])
-                                    ->searchable(),
+                Select::make('municipality')
+                    ->label('Municipality')
+                    ->options([
+                        'Atok' => 'Atok', 'Bakun' => 'Bakun', 'Bokod' => 'Bokod', 
+                        'Buguias' => 'Buguias', 'Itogon' => 'Itogon', 'Kabayan' => 'Kabayan', 
+                        'Kapangan' => 'Kapangan', 'Kibungan' => 'Kibungan', 'La Trinidad' => 'La Trinidad', 
+                        'Mankayan' => 'Mankayan', 'Sablan' => 'Sablan', 'Tuba' => 'Tuba', 'Tublay' => 'Tublay',
+                    ])
+                    ->searchable()
+                    ->required(),
 
-                                TextInput::make('no_of_rooms')
-                                    ->label('Number of Rooms')
-                                    ->numeric()
-                                    ->default(0),
+                Select::make('type')
+                    ->label('Accommodation Type')
+                    ->options([
+                        'HTL' => 'HTL - Hotel', 'RES' => 'RES - Resort', 'TIN' => 'TIN - Tourist Inn', 
+                        'APA' => 'APA - Apartel', 'PEN' => 'PEN - Pension House', 'HSS' => 'HSS - Homestay', 'OTH' => 'OTH - Others',
+                    ])
+                    ->searchable(),
 
-                                TextInput::make('male_employees')
-                                    ->label('Male Employees')
-                                    ->numeric()
-                                    ->default(0),
+                TextInput::make('no_of_rooms')
+                    ->label('Number of Rooms')
+                    ->numeric()
+                    ->default(0),
 
-                                TextInput::make('female_employees')
-                                    ->label('Female Employees')
-                                    ->numeric()
-                                    ->default(0),
-                            ]),
+                TextInput::make('male_employees')
+                    ->label('Male Employees')
+                    ->numeric()
+                    ->default(0),
 
-                        // TAB 2: GUEST DEMOGRAPHICS & METRICS
-                        \Filament\Forms\Components\Tabs\Tab::make('Guest Demographics & Metrics')
-                            ->schema([
-                                TextInput::make('ga_ph_province')
-                                    ->label('Guest Arrival (GA) - Philippine Residents (What Province)')
-                                    ->placeholder('e.g., Benguet, Cebu')
-                                    ->maxLength(255),
+                TextInput::make('female_employees')
+                    ->label('Female Employees')
+                    ->numeric()
+                    ->default(0),
 
-                                TextInput::make('ga_non_fil_country')
-                                    ->label('Guest Arrival (GA) - Non-Filipino Residents (What Country)')
-                                    ->placeholder('e.g., South Korea, Japan')
-                                    ->maxLength(255),
+                // PART 2: GUEST DEMOGRAPHICS & METRICS
+                Placeholder::make('heading_metrics')
+                    ->label('GUEST DEMOGRAPHICS & METRICS')
+                    ->content('Input guest arrivals breakdown by residency and room occupancy.'),
 
-                                TextInput::make('ga_unspecified')
-                                    ->label('Guest Arrival (GA) - Unspecified Resident')
-                                    ->numeric()
-                                    ->default(0),
+                TextInput::make('ga_ph_province')
+                    ->label('Guest Arrival (GA) - Philippine Residents (What Province)')
+                    ->placeholder('e.g., Benguet, Cebu')
+                    ->maxLength(255),
 
-                                TextInput::make('ga_overseas_filipinos')
-                                    ->label('Guest Arrival (GA) - Overseas Filipinos')
-                                    ->helperText('Filipinos born or raised in another country')
-                                    ->numeric()
-                                    ->default(0),
+                TextInput::make('ga_non_fil_country')
+                    ->label('Guest Arrival (GA) - Non-Filipino Residents (What Country)')
+                    ->placeholder('e.g., South Korea, Japan')
+                    ->maxLength(255),
 
-                                TextInput::make('gn_ph_province')
-                                    ->label('Guest Night (GN) - Philippine Residents (Province)')
-                                    ->placeholder('GA x No. of Nights')
-                                    ->maxLength(255),
+                TextInput::make('ga_unspecified')
+                    ->label('Guest Arrival (GA) - Unspecified Resident')
+                    ->numeric()
+                    ->default(0),
 
-                                TextInput::make('gn_non_fil_country')
-                                    ->label('Guest Night (GN) - Non-Filipino Residents (Country)')
-                                    ->placeholder('GA x No. of Nights')
-                                    ->maxLength(255),
+                TextInput::make('ga_overseas_filipinos')
+                    ->label('Guest Arrival (GA) - Overseas Filipinos')
+                    ->helperText('Filipinos born or raised in another country')
+                    ->numeric()
+                    ->default(0),
 
-                                TextInput::make('gn_unspecified')
-                                    ->label('Guest Night (GN) - Unspecified Resident')
-                                    ->numeric()
-                                    ->default(0),
+                TextInput::make('gn_ph_province')
+                    ->label('Guest Night (GN) - Philippine Residents (Province)')
+                    ->placeholder('GA x No. of Nights')
+                    ->maxLength(255),
 
-                                TextInput::make('gn_overseas_filipinos')
-                                    ->label('Guest Night (GN) - Overseas Filipinos')
-                                    ->numeric()
-                                    ->default(0),
+                TextInput::make('gn_non_fil_country')
+                    ->label('Guest Night (GN) - Non-Filipino Residents (Country)')
+                    ->placeholder('GA x No. of Nights')
+                    ->maxLength(255),
 
-                                TextInput::make('rooms_occupied')
-                                    ->label('No. of Rooms Occupied')
-                                    ->numeric()
-                                    ->default(0),
-                            ]),
+                TextInput::make('gn_unspecified')
+                    ->label('Guest Night (GN) - Unspecified Resident')
+                    ->numeric()
+                    ->default(0),
 
-                    ])->columnSpanFull()
+                TextInput::make('gn_overseas_filipinos')
+                    ->label('Guest Night (GN) - Overseas Filipinos')
+                    ->numeric()
+                    ->default(0),
+
+                TextInput::make('rooms_occupied')
+                    ->label('No. of Rooms Occupied')
+                    ->numeric()
+                    ->default(0),
             ]);
     }
 
