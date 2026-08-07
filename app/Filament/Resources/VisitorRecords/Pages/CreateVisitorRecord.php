778f82e3871932100edce4bbcfa0d124084a4aeb
Jan Kaisser F. Dulawan <jfd5349@students.uc-bcf.edu.ph>
2026-08-07 13:07:48 +0800

    Redirect to index after creating visitor record

--- app/Filament/Resources/VisitorRecords/Pages/CreateVisitorRecord.php
+++ app/Filament/Resources/VisitorRecords/Pages/CreateVisitorRecord.php
@@ -8,4 +8,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateVisitorRecord extends CreateRecord
 {
     protected static string $resource = VisitorRecordResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
